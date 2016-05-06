e56fac8f1273f40cd11b94ca549aa47b8e5e0520
Connor Peshek <Connorpeshek@gmail.com>
2016-05-06 13:36:14 -0500

    add coe for button

--- app.js
+++ app.js
@@ -2,8 +2,31 @@
   if (!window.addEventListener) return // Check for IE9+
 
   let options = INSTALL_OPTIONS
+  let element
 
   function updateElement() {
+    element = Eager.createElement(options.location, element)
+
+    element.className = "testing123"
+
+    const eagerTest = document.createElement("eager-test")
+
+    eagerTest.className = "testing123"
+
+    console.log(element)
+
+    eagerTest.innerHTML = `<script async="async" src="https://www.paypalobjects.com/js/external/paypal-button.min.js?merchant=RDRPHF2EZT6GJ" 
+	    data-button="buynow" 
+	    data-name="swag" 
+	    data-quantity="99999999" 
+	    data-amount="123456789" 
+	    data-currency="USD" 
+	    data-shipping="1.25" 
+	    data-tax="3.50" 
+	    data-env="sandbox"
+	></script>`
+
+    element.appendChild(eagerTest)
   }
 
   if (document.readyState === "loading") {
--- build/app.js
+++ build/app.js
@@ -4,8 +4,23 @@
   if (!window.addEventListener) return; // Check for IE9+
 
   var options = INSTALL_OPTIONS;
+  var element = void 0;
 
-  function updateElement() {}
+  function updateElement() {
+    element = Eager.createElement(options.location, element);
+
+    element.className = "testing123";
+
+    var eagerTest = document.createElement("eager-test");
+
+    eagerTest.className = "testing123";
+
+    console.log(element);
+
+    eagerTest.innerHTML = "<script async=\"async\" src=\"https://www.paypalobjects.com/js/external/paypal-button.min.js?merchant=RDRPHF2EZT6GJ\" \n\t    data-button=\"buynow\" \n\t    data-name=\"swag\" \n\t    data-quantity=\"99999999\" \n\t    data-amount=\"123456789\" \n\t    data-currency=\"USD\" \n\t    data-shipping=\"1.25\" \n\t    data-tax=\"3.50\" \n\t    data-env=\"sandbox\"\n\t></script>";
+
+    element.appendChild(eagerTest);
+  }
 
   if (document.readyState === "loading") {
     document.addEventListener("DOMContentLoaded", updateElement);

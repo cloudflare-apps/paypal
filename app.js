06c95635d55a78fbe12db037077f24fec620c5f7
Connor Peshek <Connorpeshek@gmail.com>
2016-05-06 14:46:14 -0500

    add broken code for looking over

--- app.js
+++ app.js
@@ -4,29 +4,23 @@
   let options = INSTALL_OPTIONS
   let element
 
+  const buynow = {
+    name: "Swag",
+    amount: "134.21"
+  }
+
+  const config = {
+    button: "buynow",
+    type: "button",
+    style: "primary",
+    size: "small",
+    host: "https://www.paypal.com"
+  }
+
   function updateElement() {
     element = Eager.createElement(options.location, element)
 
-    element.className = "testing123"
-
-    const eagerTest = document.createElement("eager-test")
-
-    eagerTest.className = "testing123"
-
-    console.log(element)
-
-    eagerTest.innerHTML = `<script async="async" src="https://www.paypalobjects.com/js/external/paypal-button.min.js?merchant=RDRPHF2EZT6GJ" 
-	    data-button="buynow" 
-	    data-name="swag" 
-	    data-quantity="99999999" 
-	    data-amount="123456789" 
-	    data-currency="USD" 
-	    data-shipping="1.25" 
-	    data-tax="3.50" 
-	    data-env="sandbox"
-	></script>`
-
-    element.appendChild(eagerTest)
+    paypal.button.create("RDRPHF2EZT6GJ", buynow, config, element)
   }
 
   if (document.readyState === "loading") {

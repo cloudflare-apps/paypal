9b57b5642a72650b3a980a3f5f97c778b84d4cf9
Connor Peshek <Connorpeshek@gmail.com>
2016-05-10 15:16:12 -0500

    don't allow button if merchant isn't set, fix price display bug

--- app.js
+++ app.js
@@ -8,40 +8,42 @@
   const PAYPAL_SCRIPT_URL = "https://cdn.rawgit.com/paypal/JavaScriptButtons/master/dist/button.js"
 
   function updateElements() {
-    const {buttons} = options
+	if (options.merchant) {
+		const {buttons} = options
+			buttons
+			.filter($ => $.name && $.amount && $.tax && $.shipping)
+			.forEach((attrs, i) => {
+				const paypalButton = document.createElement("script")
 
-    buttons
-    // .filter($ => $.name && $.price && $.quantity && $.tax && $.shipping && $.howOften && $.timePeriod)
-    .forEach((attrs, i) => {
-      const paypalButton = document.createElement("script")
+				const paypalPrice = document.createElement("eager-price")
 
-      const paypalPrice = document.createElement("eager-price")
+				paypalPrice.innerText = `${attrs.amount} ${attrs.currency}`
 
-      paypalPrice.innerText = attrs.amount = "undefined" ? `_ ${attrs.currency}` : `${attrs.amount} ${attrs.currency}`
+				// TODO find production host
+				paypalButton.src = `${PAYPAL_SCRIPT_URL}?merchant=${options.merchant}`
+				paypalButton.async
+				paypalButton.setAttribute("data-button", attrs.type)
+				paypalButton.setAttribute("data-type", "form")
+				paypalButton.setAttribute("data-name", attrs.name)
+				paypalButton.setAttribute("data-amount", attrs.amount)
+				paypalButton.setAttribute("data-currency", attrs.currency)
+				paypalButton.setAttribute("data-quantity-editable", 1)
+				paypalButton.setAttribute("data-tax", attrs.tax)
+				paypalButton.setAttribute("data-shipping", attrs.shipping)
+				paypalButton.setAttribute("data-size", attrs.size)
+				paypalButton.setAttribute("data-style", attrs.style)
+				if (attrs.type === "subscribe") {
+					paypalButton.setAttribute("data-recurrence", attrs.recurrence)
+					paypalButton.setAttribute("data-period", attrs.timePeriod)
+				}
 
-      // TODO find production host
-      paypalButton.src = `${PAYPAL_SCRIPT_URL}?merchant=${options.merchant}`
-      paypalButton.async
-      paypalButton.setAttribute("data-button", attrs.type)
-      paypalButton.setAttribute("data-type", "form")
-      paypalButton.setAttribute("data-name", attrs.name)
-      paypalButton.setAttribute("data-amount", attrs.amount)
-      paypalButton.setAttribute("data-currency", attrs.currency)
-      paypalButton.setAttribute("data-quantity-editable", 1)
-      paypalButton.setAttribute("data-tax", attrs.tax)
-      paypalButton.setAttribute("data-shipping", attrs.shipping)
-      paypalButton.setAttribute("data-size", attrs.size)
-      paypalButton.setAttribute("data-style", attrs.style)
-      if (attrs.type === "subscribe") {
-        paypalButton.setAttribute("data-recurrence", attrs.recurrence)
-        paypalButton.setAttribute("data-period", attrs.timePeriod)
-      }
+				const element = elements[i] = Eager.createElement(attrs.location, elements[i])
 
-      const element = elements[i] = Eager.createElement(attrs.location, elements[i])
-
-      element.appendChild(paypalPrice)
-      element.appendChild(paypalButton)
-    })
+				element.className = "eager-paypal"
+				element.appendChild(paypalPrice)
+				element.appendChild(paypalButton)
+			})
+		}
   }
 
   if (document.readyState === "loading") {
--- build/app.js
+++ build/app.js
@@ -1,71 +1,73 @@
 "use strict";
 
 (function () {
-  if (!window.addEventListener) return; // Check for IE9+
+		if (!window.addEventListener) return; // Check for IE9+
 
-  var options = INSTALL_OPTIONS;
-  var updateTimeout = void 0;
-  var delay = 1500;
-  var elements = [];
-  var PAYPAL_SCRIPT_URL = "https://cdn.rawgit.com/paypal/JavaScriptButtons/master/dist/button.js";
+		var options = INSTALL_OPTIONS;
+		var updateTimeout = void 0;
+		var delay = 1500;
+		var elements = [];
+		var PAYPAL_SCRIPT_URL = "https://cdn.rawgit.com/paypal/JavaScriptButtons/master/dist/button.js";
 
-  function updateElements() {
-    var _options = options;
-    var buttons = _options.buttons;
+		function updateElements() {
+				if (options.merchant) {
+						var _options = options;
+						var buttons = _options.buttons;
 
+						buttons.filter(function ($) {
+								return $.name && $.amount && $.tax && $.shipping;
+						}).forEach(function (attrs, i) {
+								var paypalButton = document.createElement("script");
 
-    buttons
-    // .filter($ => $.name && $.price && $.quantity && $.tax && $.shipping && $.howOften && $.timePeriod)
-    .forEach(function (attrs, i) {
-      var paypalButton = document.createElement("script");
+								var paypalPrice = document.createElement("eager-price");
 
-      var paypalPrice = document.createElement("eager-price");
+								paypalPrice.innerText = attrs.amount + " " + attrs.currency;
 
-      paypalPrice.innerText = attrs.amount = "undefined" ? "_ " + attrs.currency : attrs.amount + " " + attrs.currency;
+								// TODO find production host
+								paypalButton.src = PAYPAL_SCRIPT_URL + "?merchant=" + options.merchant;
+								paypalButton.async;
+								paypalButton.setAttribute("data-button", attrs.type);
+								paypalButton.setAttribute("data-type", "form");
+								paypalButton.setAttribute("data-name", attrs.name);
+								paypalButton.setAttribute("data-amount", attrs.amount);
+								paypalButton.setAttribute("data-currency", attrs.currency);
+								paypalButton.setAttribute("data-quantity-editable", 1);
+								paypalButton.setAttribute("data-tax", attrs.tax);
+								paypalButton.setAttribute("data-shipping", attrs.shipping);
+								paypalButton.setAttribute("data-size", attrs.size);
+								paypalButton.setAttribute("data-style", attrs.style);
+								if (attrs.type === "subscribe") {
+										paypalButton.setAttribute("data-recurrence", attrs.recurrence);
+										paypalButton.setAttribute("data-period", attrs.timePeriod);
+								}
 
-      // TODO find production host
-      paypalButton.src = PAYPAL_SCRIPT_URL + "?merchant=" + options.merchant;
-      paypalButton.async;
-      paypalButton.setAttribute("data-button", attrs.type);
-      paypalButton.setAttribute("data-type", "form");
-      paypalButton.setAttribute("data-name", attrs.name);
-      paypalButton.setAttribute("data-amount", attrs.amount);
-      paypalButton.setAttribute("data-currency", attrs.currency);
-      paypalButton.setAttribute("data-quantity-editable", 1);
-      paypalButton.setAttribute("data-tax", attrs.tax);
-      paypalButton.setAttribute("data-shipping", attrs.shipping);
-      paypalButton.setAttribute("data-size", attrs.size);
-      paypalButton.setAttribute("data-style", attrs.style);
-      if (attrs.type === "subscribe") {
-        paypalButton.setAttribute("data-recurrence", attrs.recurrence);
-        paypalButton.setAttribute("data-period", attrs.timePeriod);
-      }
+								var element = elements[i] = Eager.createElement(attrs.location, elements[i]);
 
-      var element = elements[i] = Eager.createElement(attrs.location, elements[i]);
+								element.className = "eager-paypal";
+								element.appendChild(paypalPrice);
+								element.appendChild(paypalButton);
+						});
+				}
+		}
 
-      element.appendChild(paypalPrice);
-      element.appendChild(paypalButton);
-    });
-  }
+		if (document.readyState === "loading") {
+				document.addEventListener("DOMContentLoaded", updateElements);
+		} else {
+				updateElements();
+		}
 
-  if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", updateElements);
-  } else {
-    updateElements();
-  }
+		window.INSTALL_SCOPE = {
+				setOptions: function setOptions(nextOptions) {
+						clearTimeout(updateTimeout);
+						options = nextOptions;
 
-  window.INSTALL_SCOPE = {
-    setOptions: function setOptions(nextOptions) {
-      clearTimeout(updateTimeout);
-      options = nextOptions;
+						updateTimeout = setTimeout(function () {
+								elements.forEach(function (element) {
+										return Eager.createElement(null, element);
+								});
 
-      updateTimeout = setTimeout(function () {
-        elements.forEach(function (element) {
-          return Eager.createElement(null, element);
-        });
-
-        updateElements();
-      }, delay);
-    }
-  };
+								updateElements();
+						}, delay);
+				}
+		};
 })();

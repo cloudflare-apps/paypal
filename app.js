2b9ea55b0b1ae0b3030a6e88fce686899b37ea40
Connor Peshek <Connorpeshek@gmail.com>
2016-05-11 18:17:06 -0500

    add auto commas

--- app.js
+++ app.js
@@ -22,10 +22,27 @@
   let options = INSTALL_OPTIONS
   let updateTimeout
 
+  function humanizedNumber(number = 0) {
+    const [wholes, decimals] = number.toFixed(2).split(".")
+
+    const formatted = wholes
+      .split("")
+      .reverse()
+      .reduce((accumulator, character, index, entries) => {
+        const placeIndex = index + 1
+        const delimiter = placeIndex !== entries.length && placeIndex % 3 === 0 ? "," : ""
+
+        return delimiter + character + accumulator
+      }, "")
+
+    return [formatted, decimals].join(".")
+  }
+
   function updateElements() {
     if (!options.merchant) return
 
     const {buttons} = options
+    const {region} = options
 
     buttons
     .forEach((attrs, i) => {
@@ -33,36 +50,39 @@
       const InfoWrapper = document.createElement("eager-info-wrapper")
       const ItemName = document.createElement("eager-item-name")
       const Price = document.createElement("eager-price")
+      const shippingAndTax = document.createElement("eager-shipping-and-tax")
 
       let time
 
       ItemName.innerHTML = attrs.name
 
-      Price.innerHTML = `${currencySymbol[options.region.currency]}${attrs.amount}`
+      if (toLocaleString) Price.innerHTML = `${currencySymbol[region.currency]}${attrs.amount.toLocaleString()}`
+      else Price.innerHTML = `${currencySymbol[region.currency]}${humanizedNumber(attrs.amount)}`
+
 
       if (attrs.type === "subscribe") {
         time = attrs.recurrence === 1 ? "time" : "times"
         Price.innerHTML += ` ${attrs.recurrence} ${time} ${TIME_PERIOD_SYMBOLS[attrs.timePeriod]}`
       }
 
-      if (options.region.tax && attrs.type !== "donate" || attrs.shipping && attrs.type !== "donate") {
-        const additionalCost = (options.region.tax + attrs.shipping).toFixed(2)
+      if (region.tax && attrs.type !== "donate" || attrs.shipping && attrs.type !== "donate") {
+        const additionalCost = toLocaleString ? (region.tax + attrs.shipping).toLocaleString() : humanizedNumber(region.tax + attrs.shipping)
 
         let label
 
-        if (options.region.tax && attrs.shipping) label = "shipping & tax"
-        else if (options.region.tax) label = "tax"
+        if (region.tax && attrs.shipping) label = "shipping & tax"
+        else if (region.tax) label = "tax"
         else if (attrs.shipping) label = "shipping"
 
-        Price.innerHTML += `<small> + ${currencySymbol[options.region.currency]}${additionalCost} ${label}</small>`
+        shippingAndTax.innerHTML += `<small> + ${currencySymbol[region.currency]}${additionalCost} ${label}</small>`
       }
 
       Button.src = `${PAYPAL_SCRIPT_URL}?merchant=${options.merchant}`
       Button.setAttribute("data-button", attrs.type)
       Button.setAttribute("data-type", attrs.type)
       Button.setAttribute("data-name", attrs.name)
-      Button.setAttribute("data-currency", options.region.currency)
-      Button.setAttribute("data-tax", options.region.tax)
+      Button.setAttribute("data-currency", region.currency)
+      Button.setAttribute("data-tax", region.tax)
       Button.setAttribute("data-shipping", attrs.shipping)
       Button.setAttribute("data-size", "small")
       Button.setAttribute("data-style", attrs.style)
@@ -84,6 +104,7 @@
 
       InfoWrapper.appendChild(ItemName)
       InfoWrapper.appendChild(Price)
+      InfoWrapper.appendChild(shippingAndTax)
       InfoWrapper.appendChild(Button)
       element.appendChild(InfoWrapper)
     })
--- build/app.js
+++ build/app.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
 (function () {
   if (!window.addEventListener) return; // Check for IE9+
 
@@ -24,11 +26,34 @@
   var options = INSTALL_OPTIONS;
   var updateTimeout = void 0;
 
+  function humanizedNumber() {
+    var number = arguments.length <= 0 || arguments[0] === undefined ? 0 : arguments[0];
+
+    var _number$toFixed$split = number.toFixed(2).split(".");
+
+    var _number$toFixed$split2 = _slicedToArray(_number$toFixed$split, 2);
+
+    var wholes = _number$toFixed$split2[0];
+    var decimals = _number$toFixed$split2[1];
+
+
+    var formatted = wholes.split("").reverse().reduce(function (accumulator, character, index, entries) {
+      var placeIndex = index + 1;
+      var delimiter = placeIndex !== entries.length && placeIndex % 3 === 0 ? "," : "";
+
+      return delimiter + character + accumulator;
+    }, "");
+
+    return [formatted, decimals].join(".");
+  }
+
   function updateElements() {
     if (!options.merchant) return;
 
     var _options = options;
     var buttons = _options.buttons;
+    var _options2 = options;
+    var region = _options2.region;
 
 
     buttons.forEach(function (attrs, i) {
@@ -36,34 +61,35 @@
       var InfoWrapper = document.createElement("eager-info-wrapper");
       var ItemName = document.createElement("eager-item-name");
       var Price = document.createElement("eager-price");
+      var shippingAndTax = document.createElement("eager-shipping-and-tax");
 
       var time = void 0;
 
       ItemName.innerHTML = attrs.name;
 
-      Price.innerHTML = "" + currencySymbol[options.region.currency] + attrs.amount;
+      if (toLocaleString) Price.innerHTML = "" + currencySymbol[region.currency] + attrs.amount.toLocaleString();else Price.innerHTML = "" + currencySymbol[region.currency] + humanizedNumber(attrs.amount);
 
       if (attrs.type === "subscribe") {
         time = attrs.recurrence === 1 ? "time" : "times";
         Price.innerHTML += " " + attrs.recurrence + " " + time + " " + TIME_PERIOD_SYMBOLS[attrs.timePeriod];
       }
 
-      if (options.region.tax && attrs.type !== "donate" || attrs.shipping && attrs.type !== "donate") {
-        var additionalCost = (options.region.tax + attrs.shipping).toFixed(2);
+      if (region.tax && attrs.type !== "donate" || attrs.shipping && attrs.type !== "donate") {
+        var additionalCost = toLocaleString ? (region.tax + attrs.shipping).toLocaleString() : humanizedNumber(region.tax + attrs.shipping);
 
         var label = void 0;
 
-        if (options.region.tax && attrs.shipping) label = "shipping & tax";else if (options.region.tax) label = "tax";else if (attrs.shipping) label = "shipping";
+        if (region.tax && attrs.shipping) label = "shipping & tax";else if (region.tax) label = "tax";else if (attrs.shipping) label = "shipping";
 
-        Price.innerHTML += "<small> + " + currencySymbol[options.region.currency] + additionalCost + " " + label + "</small>";
+        shippingAndTax.innerHTML += "<small> + " + currencySymbol[region.currency] + additionalCost + " " + label + "</small>";
       }
 
       Button.src = PAYPAL_SCRIPT_URL + "?merchant=" + options.merchant;
       Button.setAttribute("data-button", attrs.type);
       Button.setAttribute("data-type", attrs.type);
       Button.setAttribute("data-name", attrs.name);
-      Button.setAttribute("data-currency", options.region.currency);
-      Button.setAttribute("data-tax", options.region.tax);
+      Button.setAttribute("data-currency", region.currency);
+      Button.setAttribute("data-tax", region.tax);
       Button.setAttribute("data-shipping", attrs.shipping);
       Button.setAttribute("data-size", "small");
       Button.setAttribute("data-style", attrs.style);
@@ -83,6 +109,7 @@
 
       InfoWrapper.appendChild(ItemName);
       InfoWrapper.appendChild(Price);
+      InfoWrapper.appendChild(shippingAndTax);
       InfoWrapper.appendChild(Button);
       element.appendChild(InfoWrapper);
     });
